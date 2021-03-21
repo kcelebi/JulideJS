65ec2ec5b1833fe47201e6aead62ea0a9fe7e8e6
Kaya Celebi <kayacelebi17@gmail.com>
2021-03-21 16:54:07 -0400

    updates

--- logoscroll.js
+++ logoscroll.js
@@ -1,13 +1,22 @@
 
-let list;
+let list, imgs;
 
 function setup() {
     createCanvas(800,200);
+    list = new LinkedList(null,null);
+    for(var i=1; i < 9; i++){
+        list.push(new Node(null, loadImage('img/logo' + i + '.png')));
+    }
 }
 
 function draw() {
     background(255);
-
+    var i=0;
+    while(list.curr != null){
+        image(list.curr.value, i*60,30, 50,50);
+        list.next();
+        i++;
+    }
 }
 
 function update(){
@@ -44,5 +53,19 @@ class LinkedList {
         temp.next = None;
 
         this.head = this.head.next;
+        this.end = this.end.next;
+    }
+
+    //add to end
+    push(node) {
+        if(this.head = null){
+            this.head = node;
+            this.curr = node;
+            this.end = node;
+        }
+        else{
+            this.end.next = node;
+            this.end = this.end.next;
+        }
     }
 }
