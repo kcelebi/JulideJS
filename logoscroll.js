2c235a1aa7116c5ea4d0d69e18e17b07334c4810
Kaya Celebi <kayacelebi17@gmail.com>
2021-03-21 18:25:16 -0400

    fix for off screen

--- logoscroll.js
+++ logoscroll.js
@@ -1,12 +1,22 @@
 
-let list, imgs, len;
+let list, len, dimsx, dimsy, divs;
 
 function setup() {
-    createCanvas(800,200);
-    list = new LinkedList(null,null);
+    createCanvas(1000,200);
+
+    dimsx = [225,338,224,546,360,216,800,640];
+    dimsy = [225,149,225,155,228,206,800,480];
+    dists = []
+
     len = 8;
+    list = new LinkedList(null,null);
+    var dist = 0;
     for(var i=1; i < len+1; i++){
-        list.push(new Node(null, loadImage('img/logo' + i + '.png'), [(i-1)*60, 30]));
+        var div = dimsy[i-1]/(height-100); 
+        list.push(new Node(null, loadImage('img/logo' + i + '.png'), [dist, 50]));
+
+        dist += dimsx[i-1]/div;
+        dists.push(dimsx[i-1]/div);
     }
     list.set();
 }
@@ -14,20 +24,22 @@ function setup() {
 function draw() {
     background(255);
     for(var i=0; i < len; i++){
-        image(list.curr.value, list.curr.coord[0],list.curr.coord[1], 50,50);
-        console.log(list.curr.coord);
-        update(list.curr);
+        image(list.curr.value, list.curr.coord[0], list.curr.coord[1], dists[i], height-100);
+
+        update(list.curr,i);
         list.next();
     }
     list.begin();
     
+    line(width, 0, width, height);
+    line(0, height, width, height);
 }
 
-function update(node){
-    node.coord[0] += 1; //increment pos
+function update(node,i){
+    //node.coord[0] += 1; //increment pos
 
     if(!inBounds(node.coord[0])){
-        node.coord[0] = -50;
+        node.coord[0] = -dists[i];
     }
 
 }
