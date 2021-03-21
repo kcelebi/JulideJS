371b4fe679590a8b0c14b382a39d23561a9c5269
Kaya Celebi <kayacelebi17@gmail.com>
2021-03-20 20:44:32 -0400

    made classes

---
+++ index.html
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet">
+    </head>
+    
+    
+    <body>
+        <script src='logoscroll.js'></script>
+    </body>
+</html>
--- logoscroll.js
+++ logoscroll.js
@@ -1,12 +1,48 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet">
-    </head>
-    
-    
-    <body>
-        <script src='logoscroll.js'></script>
-    </body>
-</html>
+
+let list;
+
+function setup() {
+    createCanvas(800,200);
+}
+
+function draw() {
+    background(255);
+
+}
+
+function update(){
+
+}
+
+class Node {
+    constructor(next, value) {
+        this.next = next;
+        this.value = value;
+    }
+}
+
+class LinkedList {
+    constructor(head, end){
+        this.head = head;
+        this.end = end;
+        this.curr = head;
+    }
+
+    next() {
+        this.curr = this.curr.next;
+    }
+
+    //jump to head
+    head() {
+        this.curr = this.head;
+    }
+
+    //pop head and move to end
+    pop() {
+        var temp = this.head;
+        this.end.next = temp;
+        temp.next = None;
+
+        this.head = this.head.next;
+    }
+}
