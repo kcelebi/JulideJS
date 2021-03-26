509d0eac07c611c733a9bedae1510babe7e18e74
Kaya Celebi <kayacelebi17@gmail.com>
2021-03-25 20:45:25 -0400

    remaking original

--- logoscroll_mod.js
+++ logoscroll_mod.js
@@ -1,46 +1,8 @@
-
-let list, len, dimsx, dimsy, divs;
-
 function setup() {
-    createCanvas(1000,200);
 
-    dimsx = [225,338,224,546,360,216,800,640];
-    dimsy = [225,149,225,155,228,206,800,480];
-    dists = []
-
-    len = 8;
-    list = new LinkedList(null,null);
-    var dist = 0;
-    for(var i=1; i < len+1; i++){
-        var div = dimsy[i-1]/(height-100); 
-        list.push(new Node(null, loadImage('img/logo' + i + '.png'), [dist, 50]));
-
-        dist += dimsx[i-1]/div;
-        dists.push(dimsx[i-1]/div);
-    }
-    list.set();
 }
 
 function draw() {
-    background(255);
-    for(var i=0; i < len; i++){
-        image(list.curr.value, list.curr.coord[0], list.curr.coord[1], dists[i], height-100);
-
-        update(list.curr,i);
-        list.next();
-    }
-    list.begin();
-    
-    line(width, 0, width, height);
-    line(0, height, width, height);
-}
-
-function update(node,i){
-    node.coord[0] += 1; //increment pos
-
-    if(!inBounds(node.coord[0])){
-        node.coord[0] = -dists[i];
-    }
 
 }
 
